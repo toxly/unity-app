79be65b9aa6183b01b4379cf4a6a23d425d8ef45
Snow <toxly.lee@gmail.com>
2015-09-30 17:29:27 +0800

    Init

---
+++ Assets/Scripts/mainSceneEvent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class mainSceneEvent : MonoBehaviour
+{
+    public Button CSharpButton;
+    public Button JavaButton;
+
+	// Use this for initialization
+	void Start () {
+	    Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Init()
+    {
+        
+    }
+}
